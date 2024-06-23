0e6e242ec5737a56cf8292891202b9f3ba00d392
Joshlion525 <78796082+Joshlion525@users.noreply.github.com>
2024-06-23 14:15:35 +0100

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
 			<div className="backGround h-[300px] lg:h-[400px]">
 				<Navbar />
 			</div>
-			<div className="bg-Background min-h-screen flex flex-col items-center px-7 md:px-10 lg:px-0">
-				<div className="rounded-lg p-10 text-center relative -top-20 bg-white w-full lg:w-[850px]">
+			<div className="bg-Background min-h-screen flex flex-col items-center  px-7 md:px-10 lg:px-0">
+				<div className="rounded-lg border p-5 md:p-10 text-center relative -top-20 bg-white w-full lg:w-[850px]">
 					<div className="absolute -top-8 left-1/2 transform -translate-x-1/2">
 						<img src={Mastercraft} alt="" />
 					</div>
@@ -22,21 +22,21 @@ const Home = () => {
 						and eye strain.
 					</p>
 					<div className="flex gap-2 justify-center md:gap-0 md:justify-between">
-						<button className="bg-ButtonBackground text-white rounded-3xl text-sm md:text-base px-5 md:px-10 md:py-3 hover:bg-ButtonHover">
+						<button className="bg-ButtonBackground font-medium text-white w-full md:w-fit rounded-3xl text-sm md:text-base  md:px-10 md:py-3 hover:bg-ButtonHover">
 							Back this project
 						</button>
 						<div className="flex items-center bg-Background rounded-3xl">
 							<span className="text-Background bg-black rounded-full p-5">
 								<IoIosBookmark />
 							</span>
-							<span className="px-5 font-medium text-gray-400 hidden md:block">
+							<span className="px-5 font-bold text-gray-400 hidden md:block">
 								Bookmark
 							</span>
 						</div>
 					</div>
 				</div>
-				<div className="bg-white w-full lg:w-[850px] rounded-lg p-10">
-					<div className="flex flex-col md:flex-row gap-5 md:gap-16 text-center">
+				<div className="bg-white border w-full lg:w-[850px] rounded-lg p-5 md:p-10 relative -top-12">
+					<div className="flex flex-col md:flex-row gap-5 md:gap-16 text-center md:text-left">
 						<div className="border-b md:border-b-0 md:border-r pb-5 md:pb-0 md:pr-10">
 							<h1 className="font-bold text-4xl mb-2">$89,914</h1>
 							<p className="text-gray-400 md:text-lg font-medium">
@@ -56,9 +56,31 @@ const Home = () => {
 							</p>
 						</div>
 					</div>
-					
+					<div className="w-full">
+						<div className="w-full bg-Background rounded-full h-4 mt-10">
+							<div className="bg-ButtonBackground h-4 rounded-full w-[70%]"></div>
+						</div>
+					</div>
 				</div>
-				<div></div>
+				<div className="bg-white w-full lg:w-[850px] border relative -top-4 p-5 md:p-10 rounded-lg">
+				<h1 className="font-bold text-xl mb-5 mt-5 md:mt-0 md:mb-10">
+						About this project
+					</h1>
+					<p className="text-gray-400 md:text-lg mb-5 font-medium">
+						The Mastercraft Bamboo Monitor Riser is a sturdy and
+						stylish platform that elevates your screen to a more
+						comfortable viewing height. Placing your monitor at eye
+						level has the potential to improve your posture and make
+						you more comfortable while at work, helping you stay
+						focused on the task at hand.
+					</p>
+					<p className="text-gray-400 md:text-lg font-medium">
+						Featuring artisan craftsmanship, the simplicity of
+						design creates extra desk space below your computer to
+						allow notepads, pens, and USB sticks to be stored under
+						the stand.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
